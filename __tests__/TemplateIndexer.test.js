b673bc6d69b62a1b4031e78c6a066114b4de2105
Jeremy Nelson <jermnelson@gmail.com>
2020-09-03 15:55:47 -0600

    Adds test for a indexed template resource

--- __tests__/TemplateIndexer.test.js
+++ __tests__/TemplateIndexer.test.js
@@ -49,4 +49,127 @@ describe('Indexer', () => {
       expect(new TemplateIndexer(doc, dataset).index()).toBeNull()
     })
   })
+
+  describe('Indexing a resource template', () => {
+    const doc = {
+      'user': 'jpnelson',
+      'timestamp': '2020-01-23T03:20:45.237Z',
+      'templateId': 'sinopia:template:resource',
+      'id': 'sinopia:resourceTemplate:schema:Thing',
+      'uri': 'https://api.development.sinopia.io/repository/sinopia:resourceTemplate:schema:Thing',
+      'group': 'ld4p',
+      'types': [
+        'http://sinopia.io/vocabulary/ResourceTemplate'
+      ],
+      'data': [
+        {
+          '@id': 'https://api.development.sinopia.io/repository/sinopia:resourceTemplate:schema:Thing',
+          'http://sinopia.io/vocabulary/hasResourceTemplate': [
+            {
+              '@value': 'sinopia:template:resource'
+            }
+          ],
+          '@type': [
+            'http://sinopia.io/vocabulary/ResourceTemplate'
+          ],
+          'http://sinopia.io/vocabulary/hasResourceId': [
+            {
+              '@id': 'sinopia:resourceTemplate:schema:Thing'
+            }
+          ],
+          'http://sinopia.io/vocabulary/hasClass': [
+            {
+              '@id': 'http://schema.org/Thing'
+            }
+          ],
+          'http://www.w3.org/2000/01/rdf-schema#label': [
+            {
+              '@value': 'The most generic type of item'
+            }
+          ],
+          'http://sinopia.io/vocabulary/hasAuthor': [
+            {
+              '@value': 'Shelia Authority'
+            }
+          ],
+          'http://sinopia.io/vocabulary/hasPropertyTemplate': [
+            {
+              '@list': [
+                {
+                  '@id': '_:b12415'
+                },
+                {
+                  '@id': '_:b12417'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          '@id': '_:b12415',
+          '@type': [
+            'http://sinopia.io/vocabulary/PropertyTemplate'
+          ],
+          'http://www.w3.org/2000/01/rdf-schema#label': [
+            {
+              '@value': 'Additional Type'
+            }
+          ],
+          'http://sinopia.io/vocabulary/hasPropertyUri': [
+            {
+              '@id': 'http://schema.org/additionalType'
+            }
+          ],
+          'http://sinopia.io/vocabulary/hasPropertyAttribute': [
+            {
+              '@id': 'http://sinopia.io/vocabulary/propertyAttribute/repeatable'
+            }
+          ],
+          'http://sinopia.io/vocabulary/hasPropertyType': [
+            {
+              '@id': 'http://sinopia.io/vocabulary/propertyType/literal'
+            }
+          ]
+        },
+        {
+          '@id': '_:b12417',
+          '@type': [
+            'http://sinopia.io/vocabulary/PropertyTemplate'
+          ],
+          'http://www.w3.org/2000/01/rdf-schema#label': [
+            {
+              '@value': 'Alternative Name'
+            }
+          ],
+          'http://sinopia.io/vocabulary/hasPropertyUri': [
+            {
+              '@id': 'http://schema.org/alternateName'
+            }
+          ],
+          'http://sinopia.io/vocabulary/hasPropertyAttribute': [
+            {
+              '@id': 'http://sinopia.io/vocabulary/propertyAttribute/repeatable'
+            }
+          ],
+          'http://sinopia.io/vocabulary/hasPropertyType': [
+            {
+              '@id': 'http://sinopia.io/vocabulary/propertyType/literal'
+            }
+          ]
+        }
+      ]
+    }
+    it('indexes a resource template', async () => {
+      const dataset = await datasetFromJsonld(doc.data)
+      const templateIndexer = new TemplateIndexer(doc, dataset)
+      const indexObject = templateIndexer.index()
+      expect(indexObject.id).toBe('sinopia:resourceTemplate:schema:Thing')
+      expect(indexObject.author).toBe('Shelia Authority')
+      expect(indexObject.date).toBeUndefined()
+      expect(indexObject.resourceLabel).toBe('The most generic type of item')
+      expect(indexObject.resourceURI).toBe('http://schema.org/Thing')
+    })
+
+  })
+
 })
